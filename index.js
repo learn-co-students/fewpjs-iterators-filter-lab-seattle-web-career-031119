16c46a89257841a93ddbf955ec1477918aef1c50
Rylan Bauermeister <apple@MacBook-Pro-4.local>
2019-04-30 15:59:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, target){
+  return drivers.filter(driver => driver.toLowerCase() === target.toLowerCase())
+}
+
+function fuzzyMatch(drivers, target){
+  return drivers.filter(driver => driver.startsWith(target))
+}
+
+function matchName(drivers, target){
+  return drivers.filter(driver => driver.name === target)
+}
